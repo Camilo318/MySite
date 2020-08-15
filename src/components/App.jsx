fd43511aa0b738907c9d788a2c33a0797fd7d7ac
Camilo318 <camiloa0318@gmail.com>
2020-08-15 15:20:47 -0500

    Add a little example using Hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import data from './data.json'
 
 const App = () => {
+    const [list, setList] = useState([])
+    const handleClick = () => {
+        setList(data.loaders)
+    }
     return (
         <div>
             <h1>Billie Eilish</h1>
+            <ul>
+                {list.map(loader => (
+                    <li key={loader.id}>{loader.name}</li>
+                ))}
+            </ul>
+
+            <button onClick={handleClick}>Show Loaders</button>
         </div>
     )
 }
