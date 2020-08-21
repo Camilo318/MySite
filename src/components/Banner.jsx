444f16d76bb86b1d3b7efd0bf39b0e7b8610f682
Camilo318 <camiloa0318@gmail.com>
2020-08-20 22:00:24 -0500

    Add gsap library, remove css animation, add animation using gsap

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Banner = () => {
+    const bannerBg = React.createRef(null)
+    const bannerBgAddition = React.createRef(null)
+    useEffect(() => {
+        console.log(bannerBg.current)
+        gsap.from([bannerBg.current, bannerBgAddition.current], {
+            duration: 1.2,
+            width: 0,
+            skewX: 4,
+            ease: 'power3.inOut',
+            stagger: {
+                amount: 0.2
+            }
+        })
+    }, [])
+
     return (
         <section className="banner">
-            <div className="banner__bg"></div>
-            <div className="banner__bg--addition"></div>
+            <div className="banner__bg"
+            ref={bannerBg}></div>
+            <div className="banner__bg--addition"
+            ref={bannerBgAddition}>
+                
+            </div>
+
             <div className="banner__container">
                 <div className="banner__inner">
                     <h1>Hey, I'm Camilo</h1>
@@ -12,7 +32,7 @@ const Banner = () => {
                         I build Web Apps that deliver amazing experiences
                     </h3>
                     {/* <p>
-                        I'm a Frontend Developer that also happens to study Mechatronics Engineering. I like technology, like a lot
+                        I'm a Frontend Developer that also happens to study Mechatronics Engineering. I like technology, a lot
                     </p> */}
                     <a href="/">My Work</a>
                 </div>
