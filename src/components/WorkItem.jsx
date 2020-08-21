f7f7fea58e96de11d4ba03c0faf5a26ed6e62da5
Camilo318 <camiloa0318@gmail.com>
2020-08-21 16:44:05 -0500

    Add images folder and logic to insert the right image

--- src/components/WorkItem.jsx
+++ src/components/WorkItem.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import pokedexImg from '../assets/images/7.png'
 
 const WorkItem = (props) => {
-    const {title, des, color} = props
+    const {title, des, color, id} = props
+    const images = [pokedexImg]
     return (
         <div className='work__item' style={{backgroundColor: color}}>
             <div className="work__inner">
                 <div className="work__img">
-
+                    <img src={images[id - 1]} alt=""/>
                 </div>
                 <div className="work__description">
                     <h3>{title}</h3>
