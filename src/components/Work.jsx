644e903f01701632d9a2bc94843ec2d71f905aac
Camilo318 <camiloa0318@gmail.com>
2020-08-21 16:16:09 -0500

    Add style for WorkItem component, myData file was added

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,28 +1,10 @@
 import React from 'react'
 import WorkItem from './WorkItem'
+import data from '../myData'
 
 const Work = () => {
+    const works = data()
 
-    const works = [
-        {
-            title: 'Pokemon Game Card',
-            des: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium, facilis!',
-            id: 1,
-            color: '#ea5455'
-        },
-        {
-            title: 'Pokedex',
-            des: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium, facilis!',
-            id: 2,
-            color: '#5026a7'
-        },
-        {
-            title: 'Movie Seat Picker',
-            des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, debitis?',
-            id: 3,
-            color: '#42b883'
-        }
-    ]
     return (
         <section className='work'>
             <h2>My Work</h2>
---
+++ src/myData.js
@@ -0,0 +1,26 @@
+const data = () => {
+    const works = [
+        {
+            title: 'Pokemon Game Card',
+            des: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium, facilis!',
+            id: 1,
+            color: '#ea5455'
+        },
+        {
+            title: 'Pokedex',
+            des: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium, facilis!',
+            id: 2,
+            color: '#5026a7'
+        },
+        {
+            title: 'Movie Seat Picker',
+            des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, debitis?',
+            id: 3,
+            color: '#42b883'
+        }
+    ]
+
+    return works
+}
+
+export default data
