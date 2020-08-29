6fd5be19928e072019494df7daa75bb387c76c71
Camilo318 <camiloa0318@gmail.com>
2020-08-29 15:53:18 -0500

    Add favicon to index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 import './assets/styles/Main.scss'
+import './assets/images/favicon-16x16.png'
+import './assets/images/favicon-32x32.png'
 
 
 
