ef5163ba50c051182ebb94ee8b5f09ca88eb4708
Camilo318 <camiloa0318@gmail.com>
2020-09-05 10:05:24 -0500

    Info for the works was updated Images in .jpg were removed

--- src/myData.js
+++ src/myData.js
@@ -2,28 +2,28 @@ const data = () => {
     const works = [
         {
             title: 'Pokemon Card Game',
-            des: 'A fun memory game made with HTML/CSS and Vanilla JS. OOP approach was used to create all the logic behind',
+            des: 'A fun memory game made with HTML/CSS and Vanilla JS. OOP approach was used to create all the logic behind. Catching pokemons has never been so much fun!',
             id: 1,
             color: '#ea5455',
             link: 'https://pokecardgame.netlify.app/'
         },
         {
-            title: 'Pokedex',
-            des: 'This is not other pokedex like the ones out there. It is a SPA made with Vanilla JS, so it is blazing fast and super responsive',
+            title: 'Poke Hunt',
+            des: 'Oh boy! I kinda hit it out the park with this one. It is a SPA made with ReactJS and Redux, so it is blazing fast and super responsive. See stats, keep track of your pokemons and more! ',
             id: 2,
             color: '#5026a7',
             link: 'https://pokehunt.netlify.app/#/'
         },
         {
             title: 'Movie Seat Picker',
-            des: 'A very simple UI for picking seats in the cinema. Every movie has its own seating with selected, occupied and free spots',
+            des: 'A very simple UI for picking seats in the cinema. Every movie has its own seats with selected, occupied and free options.',
             id: 3,
             color: '#42b883',
             link: 'https://movie-seat-booking-app-blohsh.netlify.app/'
         },
         {
             title: 'Tattly',
-            des: 'Fake tattoos by real artists. A very cool page for a tatto company with very clean design',
+            des: 'Fake tattoos by real artists. A very cool page for a tatto company with a very clean and responsive design',
             id: 4,
             color: '#ffaa71',
             link: 'https://tattly.netlify.app/'
