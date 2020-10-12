02451a803ca75cc2f8eba76a67e69b50f839ba43
Camilo318 <camiloa0318@gmail.com>
2020-10-11 20:08:48 -0500

    Add terser-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+
 
 
 
@@ -13,6 +15,14 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
         publicPath: './'
+    },
+    
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin({
+            test: /\.(js|jsx)$/i,
+            exclude: /node_modules/
+        })]
     },
 
     devServer: {
