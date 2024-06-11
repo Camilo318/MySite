d36cd522ee83e02ff56821b092a6cb00634e2678
Camilo318 <camiloa0318@gmail.com>
2024-06-11 16:06:27 -0500

    update Pokehunt project

--- src/myData.js
+++ src/myData.js
@@ -1,57 +1,57 @@
 const data = () => {
-    const works = [
-        {
-            title: 'Bata-bit',
-            des: 'A landing page for a real start up. Made with the latest  toys from React and gsap to compose subtle but appealing animations throughout the website. Responsive and stunning',
-            id: 6,
-            color: '#ee6f57',
-            link: 'https://bata-bit.netlify.app/',
-            repo: 'https://github.com/Camilo318/Batata-Bit'
-        },
-        {
-            title: 'Pokemon Card Game',
-            des: 'A fun memory game made with HTML/CSS and Vanilla JS. OOP approach was used to create all the logic behind. Catching pokemons has never been so much fun!',
-            id: 3,
-            color: '#41aea9',
-            link: 'https://pokecardgame.netlify.app/',
-            repo: 'https://github.com/Camilo318/PokemonCardGame'
-        },
-        {
-            title: 'Blohsh Notes',
-            des: 'The MERN stack on all its glory. An app to create, edit and delete your notes. It uses JWT as the authorization mechanism. MongoDB stores the users and the notes, so the info is never exposed ',
-            id: 1,
-            color: '#f6830f',
-            link: 'https://blohsh-notes.herokuapp.com/',
-            repo: 'https://github.com/Camilo318/BlohshNotes'
-        },
+  const works = [
+    {
+      title: 'Bata-bit',
+      des: 'A landing page for a real start up. Made with the latest  toys from React and gsap to compose subtle but appealing animations throughout the website. Responsive and stunning',
+      id: 6,
+      color: '#ee6f57',
+      link: 'https://bata-bit.netlify.app/',
+      repo: 'https://github.com/Camilo318/Batata-Bit'
+    },
+    {
+      title: 'Pokemon Card Game',
+      des: 'A fun memory game made with HTML/CSS and Vanilla JS. OOP approach was used to create all the logic behind. Catching pokemons has never been so much fun!',
+      id: 3,
+      color: '#41aea9',
+      link: 'https://pokecardgame.netlify.app/',
+      repo: 'https://github.com/Camilo318/PokemonCardGame'
+    },
+    {
+      title: 'Blohsh Notes',
+      des: 'The MERN stack on all its glory. An app to create, edit and delete your notes. It uses JWT as the authorization mechanism. MongoDB stores the users and the notes, so the info is never exposed ',
+      id: 1,
+      color: '#f6830f',
+      link: 'https://blohsh-notes.herokuapp.com/',
+      repo: 'https://github.com/Camilo318/BlohshNotes'
+    },
 
-        {
-            title: 'Poke Hunt',
-            des: 'Oh boy! I kinda hit it out the park with this one. It is a SPA made with ReactJS and Redux, so it is blazing fast and super responsive. See stats, keep track of your pokemons and more! ',
-            id: 2,
-            color: '#1d2d50',
-            link: 'https://pokehunt.netlify.app/#/',
-            repo: 'https://github.com/Camilo318/PokeHunt'
-        },
-        {
-            title: 'Movie Seat Picker',
-            des: 'A very simple UI for picking seats in the cinema. Every movie has its own seats with selected, occupied and free options.',
-            id: 5,
-            color: '#00587a',
-            link: 'https://movie-seat-app.netlify.app/',
-            repo: 'https://github.com/Camilo318/MovieSeatBooking'
-        },
-        {
-            title: 'Blohsh Books',
-            des: 'My first FullStack Wep App. Using Express and MongoDB to create a data base that stores all the books you add on the client side in real time',
-            id: 4,
-            color: '#7579e7',
-            link: 'https://blohsh-books.herokuapp.com/',
-            repo: 'https://github.com/Camilo318/Blohsh-Books'
-        }
-    ]
+    {
+      title: 'Poke Hunt',
+      des: 'By leveraging the power of SSR, this Pokedex like app is fast and snappy. See hundreds of Pokemons along with their stats. More features are being worked on to provide and amazing Pokedex experience',
+      id: 2,
+      color: '#1d2d50',
+      link: 'https://pokehunt-ssr.vercel.app/',
+      repo: 'https://github.com/Camilo318/pokehunt-ssr'
+    },
+    {
+      title: 'Movie Seat Picker',
+      des: 'A very simple UI for picking seats in the cinema. Every movie has its own seats with selected, occupied and free options.',
+      id: 5,
+      color: '#00587a',
+      link: 'https://movie-seat-app.netlify.app/',
+      repo: 'https://github.com/Camilo318/MovieSeatBooking'
+    },
+    {
+      title: 'Blohsh Books',
+      des: 'My first FullStack Wep App. Using Express and MongoDB to create a data base that stores all the books you add on the client side in real time',
+      id: 4,
+      color: '#7579e7',
+      link: 'https://blohsh-books.herokuapp.com/',
+      repo: 'https://github.com/Camilo318/Blohsh-Books'
+    }
+  ]
 
-    return works
+  return works
 }
 
 export default data
